f63511c0c5f71c1be96e29db276efb580a929629
ayushpratap27 <ayushpratap2127@gmail.com>
2024-01-31 19:08:27 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -3,8 +3,11 @@
 let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(typeof myDate);   // o/p => object
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
