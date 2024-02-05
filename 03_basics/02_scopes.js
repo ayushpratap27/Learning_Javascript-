8df01fb1c43e17155aa6e3808367771b1be876a6
ayushpratap27 <ayushpratap2127@gmail.com>
2024-02-05 19:56:33 +0530

    Create 02_scopes.js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Ayush"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Ayush"
+    if (username === "Ayush") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)  // This is going to give error, this syntax of function declaration doesn't allow to access before initialization
+const addTwo = function(num){
+    return num + 2
+}
