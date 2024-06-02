aa636476b3de922bed3ab231bf9e8d2bc6d1b307
ayushpratap27 <ayushpratap2127@gmail.com>
2024-06-02 22:25:51 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,31 +1,34 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros) // It pushes the entire dc_heroes array as the next ele of marvel_heroes array
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros) // This merges the two array
 // console.log(allHeros);
 
+
+// Spread operator
 const all_new_heros = [...marvel_heros, ...dc_heros]    // Best method
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity) // Returns a new array with all sub-array elements concatenated into it
+// recursively up to the specifies depth
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Ayush"))
-console.log(Array.from("Ayush"))
+console.log(Array.isArray("Ayush")) // Is it array or not
+console.log(Array.from("Ayush")) // Convert it into array
 console.log(Array.from({name: "Ayush"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));// Returns a new array from a set of elements
